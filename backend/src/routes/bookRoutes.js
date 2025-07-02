2c995f615f67099dea56c66575d7cb29e0ecbbfe
iaditi <aditipython@gmail.com>
2025-07-02 14:53:29 +0530

    backend set up

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -36,11 +36,51 @@ router.get("/",protectRoute,async (req,res)=>{
         const limit = req.query.limit||5
         const skip = (page-1)*limit
         const books = await Book.find().sort({createdAt:-1}).skip(skip).limit(limit).populate("user","username profileImage")
-        res.send(books)
+        const totalBooks = await Book.countDocuments()
+        res.send({
+            books,
+            currentPage:page,
+            totalBooks,
+            totalPages:Math.ceil(totalBooks/limit)
+        })
 
     } catch (error) {
         console.log(error)
         res.status(500).json({ message: error.message });
+    }
+})
+
+router.delete("/:id",protectRoute,async (req,res)=>{
+    try {
+        const book = await Book.findById(req.params.id)
+        if(!book) return res.status(404).json({
+            message:'Book not found'
+        })
+        if(book.user.toString()!==req.user._id.toString()){
+            return res.status(401).json({message:"Unauthorized"})
+        }
+        if(book.image&& book.image.includes("cloudinary")){
+            try {
+                const publicId = book.image.split("/").pop().split(".")[0]
+                await cloudinary.uploader.destroy(publicId)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        await book.deleteOne()
+        res.json({message:"Book deleted successfully"})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: error.message });
+    }
+})
+
+router.get("/user",protectRoute,async (req,res)=>{
+    try {
+        const books = await Book.find({user:req.user._id}).sort({createdAt:-1})
+        res.json(books)
+    } catch (error) {
+        console(error)
     }
 })
 
