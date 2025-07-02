c5495655fac510c9ddc98e59207ab3d2e76d058a
iaditi <aditipython@gmail.com>
2025-07-02 14:06:33 +0530

    Book routes made

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,15 @@
 import express from "express"
 import "dotenv/config"
 import authRoutes from "./routes/authRoutes.js"
+import bookRoutes from "./routes/bookRoutes.js"
 import connectDB from "./lib/db.js"
 
 const app = express()
 const PORT = process.env.PORT
-
+app.use(express.json())
 
 app.use("/api/auth",authRoutes)
+app.use("/api/books",bookRoutes)
 
 app.listen(5010,()=>{
     console.log(`Server is running on ${PORT}`)
---
+++ backend/src/routes/bookRoutes.js
@@ -0,0 +1,47 @@
+import express from "express"
+import cloudinary from "../lib/cloudinary.js"
+import Book from "../models/Book.js"
+import protectRoute from "../middleware/auth.middleware.js"
+
+const router = express.Router()
+
+router.post("/",protectRoute,async (req,res)=>{
+    try {
+        const {title,caption,rating,image} = req.body
+        if(!title||!caption||!rating||!image){
+            return res.status(400).json({
+                message:"All fields are required"
+            })
+        }
+        const uploasResponse = await cloudinary.uploader.upload(image)
+        const imageUrl = uploasResponse.secure_url
+        const newBook = new Book({
+            title,
+            caption,
+            rating,
+            image :imageUrl,
+            user:req.user._id
+        })
+        await newBook.save()
+        res.status(201).json(newBook)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message:error.message})
+    }
+})
+
+router.get("/",protectRoute,async (req,res)=>{
+    try {
+        const page = req.query.page||1
+        const limit = req.query.limit||5
+        const skip = (page-1)*limit
+        const books = await Book.find().sort({createdAt:-1}).skip(skip).limit(limit).populate("user","username profileImage")
+        res.send(books)
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: error.message });
+    }
+})
+
+export default router
